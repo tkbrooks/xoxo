97ed194908c84657a1d1df4d693affa0476b27fb
eugenepark2014 <eugenepark2014@gmail.com>
2018-03-28 11:38:17 -0400

    first push

--- game/index.js
+++ game/index.js
@@ -1,6 +1,15 @@
 import {Map} from 'immutable'
+import { createStore } from 'redux'
 
-export default function reducer(state, action) {
+const initialState = {
+  board: Map(),
+  turn: "X"
+}
+
+const move = () => reducer.dispatch({type: "MOVE", position: [row: 0, col: 0]})
+
+export default function reducer(state = initialState, action) {
   // TODO
+
   return state
 }
